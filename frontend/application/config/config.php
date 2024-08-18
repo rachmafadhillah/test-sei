5d549b42e97eed9811ce176145f98ed66c6420f3
rachmafadhillah <168049918+rachmafadhillah@users.noreply.github.com>
2024-08-18 23:37:52 +0700

    Update config.php

--- frontend/application/config/config.php
+++ frontend/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci-application/';
+$config['base_url'] = '';
 $config['api_base_url'] = 'http://localhost:8080/'; // Replace with the actual API URL
 
 /*
